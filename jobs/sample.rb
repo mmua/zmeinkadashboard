e361630038769eaf954724b2422c79093a28dae8
Maxim Moroz <dev@leadport.ninja>
2016-02-23 10:19:08 +0000

    test sample jobs

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,13 +1,14 @@
-current_valuation = 0
-current_karma = 0
+current_humidity = 0
+current_temperature = 10
+current_voltage = 48
 
 SCHEDULER.every '2s' do
-  last_valuation = current_valuation
-  last_karma     = current_karma
-  current_valuation = rand(100)
-  current_karma     = rand(200000)
+  last_humidity = current_humidity
+  last_temperature = current_temperature
+  last_voltage = current_voltage
 
-  send_event('valuation', { current: current_valuation, last: last_valuation })
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+  send_event('humidity',    { current: current_humidity,    last: last_humidity })
+  send_event('temperature', { current: current_temperature, last: last_temperature })
+  send_event('voltage',     { current: current_voltage,     last: last_voltage })
+  #send_event('synergy',   { value: rand(100) })
 end
