7045b3b13d260c649e75f64a5fa76b5c79800fb0
Maxim Moroz <dev@leadport.ninja>
2016-02-23 11:05:09 +0000

    set meter value

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -7,8 +7,8 @@ SCHEDULER.every '2s' do
   last_temperature = current_temperature
   last_voltage = current_voltage
 
-  send_event('humidity',    { current: current_humidity,    last: last_humidity })
-  send_event('temperature', { current: current_temperature, last: last_temperature })
-  send_event('voltage',     { current: current_voltage,     last: last_voltage })
+  send_event('humidity',    { value: current_humidity })
+  send_event('temperature', { value: current_temperature })
+  send_event('voltage',     { value: current_voltage })
   #send_event('synergy',   { value: rand(100) })
 end
